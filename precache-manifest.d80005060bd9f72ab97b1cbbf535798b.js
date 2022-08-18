4e40275aff22caea3d8d97c1fe8fc61ead0447e3
dffUqp <dffuqp@gmail.com>
2022-08-19 01:21:45 +0300

    Updates

---
+++ precache-manifest.d80005060bd9f72ab97b1cbbf535798b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e1379ca9fcfdea3673c",
+    "url": "/base-react/static/css/main.6c00934e.chunk.css"
+  },
+  {
+    "revision": "6e1379ca9fcfdea3673c",
+    "url": "/base-react/static/js/main.978ceb38.chunk.js"
+  },
+  {
+    "revision": "f71d7aeb6825a1353d87",
+    "url": "/base-react/static/js/runtime~main.2a9e5094.js"
+  },
+  {
+    "revision": "1517f541248ea45c3186",
+    "url": "/base-react/static/js/2.41b6979f.chunk.js"
+  },
+  {
+    "revision": "5eb1ce774553c0d0670da1cc5e95ab00",
+    "url": "/base-react/index.html"
+  }
+];
